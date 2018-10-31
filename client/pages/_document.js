b2b529f9d31cbc783e21ce23f156d735be5508d7
Thomas Lindstrøm <t@hom.as>
2018-10-31 11:05:10 +0100

    Add `Dockerfile`, linting

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,21 +1,21 @@
+import React from 'react';
 import Document, {Head, Main, NextScript} from 'next/document';
 
 export default class CustomDocument extends Document {
-    render() {
-       return (
-            <html id="document" lang="en">
-                <Head>
-                    <meta charSet="utf-8" />
-                    <link rel="icon" href="/static/favicon.png" />
-                </Head>
+	render() {
+		return (
+			<html id="document" lang="en">
+				<Head>
+					<meta charSet="utf-8"/>
+					<link rel="icon" href="/static/favicon.png"/>
+				</Head>
 
-                <body>
-                    <Main />
-
-                    <script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
-                    <NextScript />
-                </body>
-            </html>
-        );
-    }
+				<body>
+					<Main/>
+					<script src="https://cdn.polyfill.io/v2/polyfill.min.js"/>
+					<NextScript/>
+				</body>
+			</html>
+		);
+	}
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,7 @@
-import './_document.css';
+import React from 'react';
 import classNames from './index.css';
+
+import('./_document.css');
 
 export default () => {
 	console.log('it works');
