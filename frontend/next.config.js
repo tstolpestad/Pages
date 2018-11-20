c7904ff97732a7a1dde27f51b320a2deb7dd0913
Thomas Lindstrøm <t@hom.as>
2018-11-20 16:04:53 +0100

    Fix deployment issue

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,29 @@
+let PHASE_PRODUCTION_SERVER;
+
+try {
+	({PHASE_PRODUCTION_SERVER} = require('next-server/constants'));
+} catch (error) {
+	({PHASE_PRODUCTION_SERVER} = require('next/constants'));
+}
+
 const config = {
 	serverRuntimeConfig: {},
 	publicRuntimeConfig: {}
 };
 
-module.exports = Object.assign(
-	config,
-	require('@zeit/next-css')({
-		cssModules: true,
-		cssLoaderOptions: {
-			importLoaders: 1,
-			localIdentName: '[local]-[hash:base64:5]'
-		}
-	})
-);
+module.exports = (phase) => {
+	if (phase === PHASE_PRODUCTION_SERVER) {
+		return config;
+	}
+
+	return Object.assign(
+		config,
+		require('@zeit/next-css')({
+			cssModules: true,
+			cssLoaderOptions: {
+				importLoaders: 1,
+				localIdentName: '[local]-[hash:base64:5]'
+			}
+		})
+	);
+};
